c2dd794b7415ee6ae77e61b0125a3fff8317c27c
zjw977020546 <11487928+zjw977020546@user.noreply.gitee.com>
2023-06-01 21:09:28 +0800

    first commit

--- src/.vuepress/client.ts
+++ src/.vuepress/client.ts
@@ -10,14 +10,12 @@ import {
   setupRibbon,
 } from './composables/index.js';
 
-const MyLinks = defineAsyncComponent(() => import('./components/MyLinks.vue'));
 const MyIcon = defineAsyncComponent(() => import('./components/MyIcon.vue'));
 const HeroBG = defineAsyncComponent(() => import('./components/HeroBG.vue'));
 const NavMusic = defineAsyncComponent(() => import('./components/NavMusic.vue'));
 
 export default defineClientConfig({
   enhance({ app }) {
-    app.component('MyLinks', MyLinks);
     app.component('MyIcon', MyIcon);
   },
   setup() {
